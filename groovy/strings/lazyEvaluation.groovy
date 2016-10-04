300c45f37f40db7de3f0e02e7c1e05ce8c400168
KC Wayman <kc.wayman@appian.com>
2016-10-03 20:56:33 -0400

    [groovy] heredoc example

--- groovy/strings/lazyEvaluation.groovy
+++ groovy/strings/lazyEvaluation.groovy
@@ -22,4 +22,15 @@ assert 'cow jumped tiny bear' == betterStr
 // giveMeWhat.toString(), it is actually doing
 // giveMeWhat.call().toString() implicitly, because it is "smart"
 
+// Additionally, it's totally valid to use heredoc syntax!
+word1 = 'reticent'
+word2 = 'perchance'
+word3 = 'thrill'
+superLongMissive = """I am pretty pleased with the length of this message, but to be honest I am somewhat $word1 to send it.
+I feel like I could $word2 be judged on its contents, a prospect to which I do not $word3.
+Oh well.
+Sending.
+"""
+println superLongMissive
+
 println "All assertions passed"
